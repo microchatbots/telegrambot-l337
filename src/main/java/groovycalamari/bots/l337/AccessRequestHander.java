c83efb4b2d9ebc108baff2dd86faa325df08b1ba
Sergio del Amo <sergio.delamo@softamo.com>
2021-02-14 23:30:46 +0100

    log: add log statement about info

--- src/main/java/groovycalamari/bots/l337/AccessRequestHander.java
+++ src/main/java/groovycalamari/bots/l337/AccessRequestHander.java
@@ -1,18 +1,21 @@
 package groovycalamari.bots.l337;
 
-
 import com.microchatbots.core.parser.SpaceParser;
 import com.microchatbots.core.parser.TextParser;
 import com.microchatbots.core.parser.UserParser;
 import com.microchatbots.telegrambots.api.TelegramBotConfiguration;
 import com.microchatbots.telegrambots.core.Update;
 import com.microchatbots.telegrambots.handler.SendMessageRequestHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javax.inject.Singleton;
 import java.io.Serializable;
 import java.util.Optional;
 
 @Singleton
 public class AccessRequestHander extends SendMessageRequestHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(AccessRequestHander.class);
+
     private final UserParser<Update> userParser;
     private final L337Configuration l337Configuration;
 
@@ -23,8 +26,6 @@ public class AccessRequestHander extends SendMessageRequestHandler {
         super(spaceParser, textParser);
         this.userParser = userParser;
         this.l337Configuration = l337Configuration;
-
-
     }
 
     @Override
@@ -37,6 +38,9 @@ public class AccessRequestHander extends SendMessageRequestHandler {
         Optional<Serializable> userIdOptional = userParser.parseUserUniqueIdentifier(telegramBotConfiguration, update);
         if (userIdOptional.isPresent()) {
             String userId = userIdOptional.get().toString();
+            if (LOG.isInfoEnabled()) {
+                LOG.info("user id: {}", userId);
+            }
             return !l337Configuration.getAllowedUsers().contains(userId);
         }
         return true;
